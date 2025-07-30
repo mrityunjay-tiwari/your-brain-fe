a0148ff631a6bf9e8f3ffbba33053fd61f21d19a
mrityunjay_ <mrityunjaytiwari1873@gmail.com>
2025-07-30 17:19:29 +0530

    minor update

--- src/components/ui/navBar.tsx
+++ src/components/ui/navBar.tsx
@@ -1,6 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
 import { Logo } from "../../icons/logo";
-import { Button } from "./button";
 import { Button2 } from "./button2";
 
 export function NavBar(){
