0b31694e2f19ae3554afacb0f2a83bfad94e89cd
mrityunjay_ <mrityunjaytiwari1873@gmail.com>
2025-07-30 17:22:05 +0530

    minor update 2

--- src/components/ui/sideBarElements.tsx
+++ src/components/ui/sideBarElements.tsx
@@ -5,7 +5,7 @@ interface propsType{
     text?: string
 }
 export function SideBarElement({icon, text} : propsType) {
-    return <div className="flex gap-4 text-lg font-light items-center py-2 hover:bg-slate-100 hover:cursor-pointer transition-all justify-center sm:justify-start sm:pl-6">
+    return <div className="flex gap-4 text-lg font-light items-center py-2 sm:hover:bg-slate-100 hover:cursor-pointer transition-all justify-center sm:justify-start sm:pl-6">
         <div>{icon}</div>
         <div className="hidden sm:inline">{text}</div>
 
