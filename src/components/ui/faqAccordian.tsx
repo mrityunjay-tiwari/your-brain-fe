b709960c86f6855fcaf5accbb1406a311f87c774
mrityunjay_ <mrityunjaytiwari1873@gmail.com>
2025-07-30 14:08:54 +0530

    revised changes

--- src/components/ui/faqAccordian.tsx
+++ src/components/ui/faqAccordian.tsx
@@ -1,10 +1,8 @@
 import Accordion from '@mui/material/Accordion';
-import AccordionActions from '@mui/material/AccordionActions';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Button from '@mui/material/Button';
 
 export function AccordionUsage() {
   return (
